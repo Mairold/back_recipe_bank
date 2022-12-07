5282417b904fb7f49f3f7875563a205c6528d118
Mairold <mairold.maegi@gmail.com>
2022-12-07 18:25:33 +0200

    Develope in progress

--- src/main/java/ee/recipebank/backrecipebank/domain/ingridient/recipeingredient/RecipeIngredientRepository.java
+++ src/main/java/ee/recipebank/backrecipebank/domain/ingridient/recipeingredient/RecipeIngredientRepository.java
@@ -1,11 +1,6 @@
-package ee.recipebank.backrecipebank.domain.ingridient.recipeingredient;
+package ee.recipebank.backrecipebank.domain.ingridient.recipeIngredient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Integer> {
-    @Query("select r from RecipeIngredient r where r.recipe.id = ?1 order by r.ingredient.ingredientGroup.name")
-    List<RecipeIngredient> findRecipeIngredientsBy(Integer recipeId);
 }
