0955782f1dae9efdb5a122d765fa40e54cd9e33e
Mairold <mairold.maegi@gmail.com>
2022-12-07 18:37:52 +0200

    Develope in progress

--- src/main/java/ee/recipebank/backrecipebank/business/ingredient/dto/AllowedMeasurementUnitDto.java
+++ src/main/java/ee/recipebank/backrecipebank/business/ingredient/dto/AllowedMeasurementUnitDto.java
@@ -1,7 +1,9 @@
 package ee.recipebank.backrecipebank.business.ingredient.dto;
 
 import ee.recipebank.backrecipebank.domain.ingridient.allowedmeasurements.AllowedMeasurementUnit;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -11,10 +13,12 @@ import java.io.Serializable;
  * A DTO for the {@link AllowedMeasurementUnit} entity
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllowedMeasurementUnitDto implements Serializable {
-    private final Integer allowedMeasurementUnitId;
-    private final Integer measurementUnitId;
+    private  Integer allowedMeasurementUnitId;
+    private Integer measurementUnitId;
     @Size(max = 50)
     @NotNull
-    private final String allowedMeasurementName;
+    private String allowedMeasurementName;
 }
---
+++ src/main/java/ee/recipebank/backrecipebank/domain/ingridient/recipeingredient/RecipeIngredientMapper.java
@@ -0,0 +1,29 @@
+package ee.recipebank.backrecipebank.domain.ingridient.recipeingredient;
+
+import ee.recipebank.backrecipebank.business.ingredient.dto.RecipeIngredientDto;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public interface RecipeIngredientMapper {
+//    @Mapping(source = "ingredientName", target = "ingredient.name")
+//    @Mapping(source = "measureUnitName", target = "measureUnit.name")
+//    RecipeIngredient recipeIngredientDtoToRecipeIngredient(RecipeIngredientDto recipeIngredientDto);
+
+//    @InheritInverseConfiguration(name = "recipeIngredientDtoToRecipeIngredient")
+//    RecipeIngredientDto recipeIngredientToRecipeIngredientDto(RecipeIngredient recipeIngredient);
+//
+//    @InheritConfiguration(name = "recipeIngredientDtoToRecipeIngredient")
+//    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+//    RecipeIngredient updateRecipeIngredientFromRecipeIngredientDto(RecipeIngredientDto recipeIngredientDto, @MappingTarget RecipeIngredient recipeIngredient);
+
+    @Mapping(source = "ingredient.name", target = "ingredientName")
+    @Mapping(source = "quantity", target = "quantity")
+    @Mapping(source = "measureUnit.name", target = "measureUnitName")
+    RecipeIngredientDto toDto(RecipeIngredient recipeIngredient);
+
+    List<RecipeIngredientDto> toDtos(List<RecipeIngredient> recipeIngredients);
+
+
+}
